6eab57d3c396f2078e80f2fc441d3b2302b5333b
Jopt05 <56639785+Jopt05@users.noreply.github.com>
2024-05-01 15:01:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-primer-ejemplo
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Jopt05/angular-primer-ejemplo)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { products, Product } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productId = Number(routeParams.get('productId'));
+
+    this.product = products.find((product) => product.id == productId);
+  }
+
+  handleGoBack() {
+    this.router.navigate(['/']);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Added to cart!');
+  }
+}
